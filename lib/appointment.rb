fff0ae3d042301449a6bc932d95d06a5b010ee3f
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-15 11:32:39 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,8 +1,16 @@
 class Appointment
-  attr_accessor :date, :doctor
+  attr_accessor :date, :doctor, :patient
 
 def initialize(date, doctor)
   @date = date 
+  @doctor = doctor
+end
+
+def patient
+  @patient = self
+end
+
+def doctor
   @doctor = doctor
 end
 
